a46a0b11a8edb43653b19a368107d8c024b96f93
suziwen <suziwen1@gmail.com>
2020-04-07 23:04:39 +0800

    fix

--- src/components/Gitalk.jsx
+++ src/components/Gitalk.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import GitalkComponent from "gitalk/dist/gitalk-component"
-import { insertScript, removeScript, shallowComparison } from '../utils'
 
 export default class GGitalk extends React.Component {
   
   constructor(props) {
     super(props)
-    const configStr = (typeof GATSBY_GITALK_CONFIG !== `undefined` && GATSBY_GITALK_CONFIG !== '') ? GATSBY_GITALK_CONFIG : ''
-    if (configStr) {
-      try {
-        this.pluginConfig = JSON.parse(configStr)
-      } catch (e) {
-        this.pluginConfig = null
-      }
-    }
+    this.pluginConfig = (typeof GATSBY_GITALK_CONFIG !== `undefined` && GATSBY_GITALK_CONFIG !== '') ? GATSBY_GITALK_CONFIG : ''
   }
   render() {
     let { options, ...props } = this.props
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,11 +1,13 @@
 'use strict'
 
 var didRunAlready = false;
+var pluginConfig = "";
 
 exports.onPreInit = function(_ref, pluginOptions) {
   
   // Gatsby adds a pluginOptions that's not needed for this plugin
   delete pluginOptions.plugins
+  pluginConfig = pluginOptions.config
 
   if (didRunAlready) {
     throw new Error('You can only have single instance of gatsby-plugin-gitalk in your gatsby-config.js')
@@ -17,7 +19,7 @@ exports.onCreateWebpackConfig = ({ plugins, actions, }) => {
   var setWebpackConfig = actions.setWebpackConfig
   setWebpackConfig({
     plugins: [plugins.define({
-      GATSBY_GITALK_CONFIG: JSON.stringify(pluginOptions.config)
+      GATSBY_GITALK_CONFIG: JSON.stringify(pluginConfig)
     })]
   })
 }
